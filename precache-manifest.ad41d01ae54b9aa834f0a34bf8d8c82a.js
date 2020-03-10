96752e97020ef5c3014c79a835118ee3316ba158
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-10 03:20:07 +0000

    Deploy dev-launchers-sandbox/project__sports-area to github.com/dev-launchers-sandbox/project__sports-area.git:gh-pages

---
+++ precache-manifest.ad41d01ae54b9aa834f0a34bf8d8c82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5df0186b03e59dce3cd74aa07006bde",
+    "url": "/dev/project__sports-area/index.html"
+  },
+  {
+    "revision": "da9197cc59ab4d15549d",
+    "url": "/dev/project__sports-area/static/css/main.6e8cf7ca.chunk.css"
+  },
+  {
+    "revision": "11875e3af915caea0a04",
+    "url": "/dev/project__sports-area/static/js/2.358d5879.chunk.js"
+  },
+  {
+    "revision": "da9197cc59ab4d15549d",
+    "url": "/dev/project__sports-area/static/js/main.e453dab0.chunk.js"
+  },
+  {
+    "revision": "c80903b8c60d7715e4ab",
+    "url": "/dev/project__sports-area/static/js/runtime~main.af0af618.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dev/project__sports-area/precache-manifest.ad41d01ae54b9aa834f0a34bf8d8c82a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev/project__sports-area/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
